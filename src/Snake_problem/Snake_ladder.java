a3e1b428dfe94be95a428ac9dd36ca8ecf3316e2
DhanashriNerkar <dhanashrinerkar2@gmail.com>
2022-04-25 00:02:25 +0530

    UC2 and UC3 added

--- src/Snake_problem/Snake_ladder.java
+++ src/Snake_problem/Snake_ladder.java
@@ -2,10 +2,24 @@ package Snake_problem;
 
 public class Snake_ladder
 {
-	
+	public static int rollDie() 
+	{
+		int var0 = (int)(Math.random() * 6.0D + 1.0D);
+		System.out.println("The die value is " + var0);
+		return var0;
+	}
+
+	public static int optionValue() 
+	{
+		int var0 = (int)(Math.random() * 3.0D + 1.0D);
+		return var0;
+	}
 	public static int playTurn(int var0)
 	{
-	
+		int var1 = rollDie();
+		int var2 = optionValue();
+		
+		
 		System.out.println("Player position: " + var0);
 		return var0;
 	}
